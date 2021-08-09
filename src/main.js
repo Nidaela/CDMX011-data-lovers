1fc7215168f337457ae85653ad36265c190a6772
Nidaela <daniela.rmcs@gmail.com>
2021-08-09 17:29:54 -0500

    muestra titulos de peliculas en pantalla

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+const myDOM = document.getElementById('root');
+
+data.films.forEach(eachfilm => {
+    const div = document.createElement("div")
+    div.innerHTML = myHTML(eachfilm);
+    myDOM.appendChild(div);
+})
+
+function myHTML (movie) {
+    let html = `
+    <image src "${movie.poster}" height = "200px"; width = "150px";/>
+    <p>${movie.title}/<p>
+    `
+    return html
+}
